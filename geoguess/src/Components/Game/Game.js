dcb5dceefe2b959019d1ab86ea97ca015e8eb8ce
Pedro Chang <pedrochang93@gmail.com>
2022-03-11 16:07:11 -0500

    styling added

--- geoguess/src/Components/Game/Game.js
+++ geoguess/src/Components/Game/Game.js
@@ -1,91 +1,100 @@
-import './Game.scss'
-import React from 'react'
-import axios from 'axios'
-const PORT = '8080'
-const apiURL = `http://localhost:${PORT}`
+import "./Game.scss";
+import React from "react";
+import axios from "axios";
+const PORT = "8080";
+const apiURL = `http://localhost:${PORT}`;
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      question: [],
-      current: 1,
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			question: [],
+			current: 1,
+			guess: true,
+		};
+	}
 
-  fetchQuestionInfo(current) {
-    //this.props.history.push(`/game/${this.state.current}`)
-    //console.log(this.state.current)
-    axios
-      .get(`${apiURL}/game/${current}`)
-      .then((response) => {
-        console.log(response)
-        this.setState({
-          question: response.data,
-        })
-      })
-      .catch((err) => {
-        console.log('question fetch' + err)
-      })
-  }
+	fetchQuestionInfo(current) {
+		//this.props.history.push(`/game/${this.state.current}`)
+		//console.log(this.state.current)
+		axios
+			.get(`${apiURL}/game/${current}`)
+			.then(response => {
+				console.log(response);
+				this.setState({
+					question: response.data,
+				});
+			})
+			.catch(err => {
+				console.log("question fetch" + err);
+			});
+	}
 
-  componentDidMount() {
-    this.fetchQuestionInfo(1)
-  }
+	componentDidMount() {
+		this.fetchQuestionInfo(1);
+	}
 
-  componentDidUpdate(prevProps) {
-    //console.log(this.props.match)
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      //console.log(this.props.match.params.id)
-      this.fetchQuestionInfo(this.props.match.params.id)
-    }
-  }
-  handleSubmit = (ev) => {
-    ev.preventDefault()
-    console.log(ev.target.guess.value)
-    console.log(this.state.question.name)
-    if (ev.target.guess.value === this.state.question.name) {
-      // this.setState(
-      //   {
-      //     current: this.state.current + 1,
-      //   },
-      //   () => {
-      //     this.props.history.push(this.state.current)
-      //   }
-      // )
-      this.props.history.push(`/game/${Number(this.props.match.params.id) + 1}`)
-      console.log('right')
-      ev.target.reset()
-    } else {
-      console.log('wrong')
-    }
-  }
+	componentDidUpdate(prevProps) {
+		//console.log(this.props.match)
+		if (prevProps.match.params.id !== this.props.match.params.id) {
+			//console.log(this.props.match.params.id)
+			this.fetchQuestionInfo(this.props.match.params.id);
+		}
+	}
+	handleSubmit = ev => {
+		ev.preventDefault();
+		// console.log(ev.target.guess.value);
+		// console.log(this.state.question.name);
+		console.log(this.props.match);
+		if (ev.target.guess.value.toUpperCase() === this.state.question.name) {
+			// this.setState(
+			//   {
+			//     current: this.state.current + 1,
+			//   },
+			//   () => {
+			//     this.props.history.push(this.state.current)
+			//   }
+			// )
+			this.props.history.push(
+				`/game/${Number(this.props.match.params.id) + 1}`
+			);
+			console.log("right");
+			this.setState({ guess: true });
+			ev.target.reset();
+		} else {
+			this.setState({ guess: false });
+			console.log("wrong");
+		}
+	};
 
-  render() {
-    return (
-      <div className='game'>
-        <div className='game-pholder'>
-          <img
-            className='game-pholder__img'
-            alt='main game pic'
-            src={`${apiURL}/${this.state.question.image}`}
-          />
-        </div>
-        <form className='game-text' onSubmit={this.handleSubmit}>
-          <h3 className='game-text__title'> guess this place </h3>
-          <input
-            name='guess'
-            className='game-text__input'
-            placeholder={this.state.question.name}
-            type='text'
-          ></input>
-          <button className='nav-bar__play' type='submit'>
-            Guess!
-          </button>
-        </form>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className='game'>
+				<div className='game-pholder'>
+					<img
+						className='game-pholder__img'
+						alt='main game pic'
+						src={`${apiURL}/${this.state.question.image}`}
+					/>
+				</div>
+				<div className='right-wrapper'>
+					<form className='game-text' onSubmit={this.handleSubmit}>
+						<h3 className='game-text__title'> Guess this place </h3>
+						<input
+							name='guess'
+							className='game-text__input'
+							placeholder='Try to guess'
+							type='text'
+						></input>
+						<button className='nav-bar__play' type='submit'>
+							Guess!
+						</button>
+					</form>
+					{!this.state.guess ? <p className='error-guess'>Guess again!</p> : ""}
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Game
+export default Game;
--- geoguess/src/Components/Home/Home.js
+++ geoguess/src/Components/Home/Home.js
@@ -1,18 +1,19 @@
+import "./Home.scss";
 import { Link } from "react-router-dom";
 
 function Home() {
-  return (
-    <div className="home">
-      <h1 className="home__title"> Welcome to our game!</h1>
-      <h4 className="home__description">
-        {" "}
-        Try to guess all the famous places{" "}
-      </h4>
+	return (
+		<div className='home'>
+			<h1 className='home__title'> Welcome to our game!</h1>
+			<h4 className='home__description'>
+				{" "}
+				Try to guess all the famous places{" "}
+			</h4>
 
-      <Link to={`/game`}>
-        <button className="home__play">PLAY!</button>
-      </Link>
-    </div>
-  );
+			<Link to={`/game/1`}>
+				<button className='home__play'>PLAY!</button>
+			</Link>
+		</div>
+	);
 }
 export default Home;
